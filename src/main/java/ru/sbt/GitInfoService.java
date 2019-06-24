2d8859109253619a51d77a81f01e0363fb242b7b
EnjeruGorkaya <tanya.meow13@gmail.com>
2019-06-24 03:27:55 +0300

    GitInfo сохранение путей

--- src/main/java/ru/sbt/GitInfoService.java
+++ src/main/java/ru/sbt/GitInfoService.java
@@ -1,15 +1,23 @@
 package ru.sbt;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class GitInfoService {
+    private String pathToRepository;
 
-    public void getCommitInfo(String path) throws IOException {
-        String command = "git log --output filenameForEx.txt --name-status";
-        File file = new File(path);
+    public GitInfoService(String pathToRepository) {
+        this.pathToRepository = pathToRepository;
+    }
+
+    public void getCommitInfo() throws IOException {
+        String command = "git log --output gitLog.txt --name-status --pretty=format:\"\"";
 
         Runtime runtime = Runtime.getRuntime();
-        Process proc = runtime.exec(command, null, file);
+        Process proc = runtime.exec(command, null, new File(pathToRepository));
 
         InputStream is = proc.getInputStream();
         InputStreamReader isr = new InputStreamReader(is);
@@ -20,4 +28,35 @@ public class GitInfoService {
             System.out.println(line);
         }
     }
+
+    public void readFileToMap() {
+        Map<String, Integer> map = new HashMap();
+        List<String> testList = new ArrayList<>();
+        try {
+            FileInputStream fstream = new FileInputStream(pathToRepository + "\\gitLog.txt");
+            String pathBeforeDir = pathToRepository.substring(0, pathToRepository.lastIndexOf('\\'));
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String strLine;
+            while ((strLine = br.readLine()) != null) {
+                if (!strLine.equals("")) {
+                    // отделяю вид коммита
+                    String[] addresses = strLine.split("\t");
+                    String address = pathToRepository + '\\' + addresses[1];
+                    testList.add(address);
+                    // если ээлемент есть, увеличиваем счетчик
+                    Integer amount = map.get(address);
+                    if (amount != null) {
+                        map.put(address, amount + 1);
+                    } else {
+                        map.put(address, 1);
+                    }
+                }
+            }
+            System.out.println(map.get("C:\\Users\\Enjeru\\MIPT\\Diplom\\ForAnalysis\\cassandra\\src\\java\\org\\apache\\cassandra\\service\\CacheServiceMBean.java"));
+        } catch (IOException e) {
+            System.out.println("Ошибка Чтения/записи файла gitLog.txt\"");
+        }
+    }
+
+
 }
--- src/test/java/ru/sbt/GitInfoServiceTest.java
+++ src/test/java/ru/sbt/GitInfoServiceTest.java
@@ -4,14 +4,13 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-import static org.junit.Assert.*;
-
 public class GitInfoServiceTest {
 
     @Test
     public void test() throws IOException {
-        GitInfoService service = new GitInfoService();
-        service.getCommitInfo("C:\\Users\\Enjeru\\MIPT\\Diplom\\ForAnalysis\\microconfig\\microconfig");
+        GitInfoService service = new GitInfoService("C:\\Users\\Enjeru\\MIPT\\Diplom\\ForAnalysis\\cassandra");
+        service.getCommitInfo();
+        service.readFileToMap();
     }
 
 }
