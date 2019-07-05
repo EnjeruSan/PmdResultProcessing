4e023758bbcf526472dc9fabb6d58e19ac43bc57
EnjeruGorkaya <tanya.meow13@gmail.com>
2019-07-05 04:01:07 +0300

    -

---
+++ src/main/java/ru/sbt/SolidMetrics.java
@@ -0,0 +1,16 @@
+package ru.sbt;
+
+public class SolidMetrics {
+    private Double srp;
+    private Double ocp;
+    private Double lsp;
+    private Double isp;
+    private Double dip;
+
+    SolidMetrics(RulesInfo rulesInfo) {
+//        Double srp_arg = 0.0003 + rulesInfo.
+//        srp = 1/(1 + Math.exp(-1 * ));
+    }
+
+
+}
--- src/main/java/ru/sbt/TextWriteService.java
+++ src/main/java/ru/sbt/TextWriteService.java
@@ -1,17 +1,31 @@
 package ru.sbt;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Set;
 
 public class TextWriteService implements ResultWriteService {
 
-    String inputFileName;
+    String outputFileName;
 
-    public TextWriteService(String inputFileName) {
-        this.inputFileName = inputFileName;
+    public TextWriteService(String outputFileName) {
+        this.outputFileName = outputFileName;
     }
 
     @Override
     public void writeResult(Set<ClassMetaData> metaDataMap) {
 
+        for (ClassMetaData metaData : metaDataMap) {
+
+        }
+
+        try {
+            FileOutputStream out = new FileOutputStream(new File(outputFileName));
+            //workbook.write(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+    
 }
